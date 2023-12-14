c7e3579a735aa394a728e8fcf0e4c13d9daa0566
stombreake <47228436+stombreake@users.noreply.github.com>
2023-12-14 12:23:31 -0600

    Ultimo Commit

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.26.3 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
@@ -26,7 +27,7 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.13.1 // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
---
+++ routers/registro.go
@@ -0,0 +1,70 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/tiwtterGo/bd"
+	"github.com/tiwtterGo/models"
+)
+
+func Registro(ctx context.Context) models.RespApi {
+
+	var t models.Usuario
+	var r models.RespApi
+	r.Status = 400
+
+	fmt.Println("Entre al registro")
+
+	body := ctx.Value(models.Key("body")).(string)
+	err := json.Unmarshal([]byte(body), &t)
+
+	if err != nil {
+		r.Message = err.Error()
+		fmt.Println(r.Message)
+		return r
+	}
+
+	if len(t.Email) == 0 {
+		r.Message = "Debe especificar el email"
+
+		fmt.Println(r.Message)
+		return r
+	}
+	if len(t.Password) < 6 {
+		r.Message = "Debe especificar una contraseña de almenos 6 caracteres"
+
+		fmt.Println(r.Message)
+		return r
+	}
+
+	_, encontrado, _ := bd.ChequeoYaExisteUsuario(t.Email)
+
+	if encontrado {
+		r.Message = "Ya existe un usuario registrado con ese email"
+		fmt.Println(r.Message)
+		return r
+	}
+
+	_, status, err := bd.InsertoRegistro(t)
+
+	if err != nil {
+		r.Message = "Ocurrio un error al intentar realizar el registro del usuario" + err.Error()
+		fmt.Println(r.Message)
+		return r
+
+	}
+
+	if !status {
+		r.Message = "No se ha logrado insertar el registro del usuario"
+		fmt.Println(r.Message)
+		return r
+	}
+
+	r.Status = 200
+	r.Message = "Registro OK"
+	fmt.Println(r.Message)
+	return r
+
+}
